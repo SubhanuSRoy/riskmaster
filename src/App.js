5cb1d2ca3c13848d835f28f11f56b097e2ce0031
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2023-11-30 18:29:47 +0530

    ft. project setup

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { Suspense, useEffect, useState } from "react";
+
+import "./App.css";
+import Loader from "./Components/Common/Loader";
+import DefaultLayout from "./Components/DefaultLayout/DefaultLayout";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+
+import routes from "./routes";
+import SignIn from "./pages/Auth/SignIn";
+import { useSelector } from "react-redux";
+import Converse from "./pages/Chat/Converse";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
+  console.log(isLoggedIn);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      {!isLoggedIn ? (
+        <Routes>
+          <Route index element={<SignIn />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        </Routes>
+      ) : (
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<Converse />} />
+            {routes.map(({ path, component: Component }) => (
+              <Route
+                path={path}
+                element={
+                  <Suspense fallback={<Loader />}>
+                    <Component />
+                  </Suspense>
+                }
+              />
+            ))}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 }
 
