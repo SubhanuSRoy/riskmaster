8b4194778260fcd68c34f2fdfccd207a3f2567e4
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2023-11-30 19:46:36 +0530

    ft. dashboard ui

---
+++ src/Components/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Header from "../Header/Header";
+import Sidebar from "../Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
+
+
+const DefaultLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="bg-boxdark-2 text-bodydark">
+      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      <div className="flex h-screen overflow-hidden">
+        {/* <!-- ===== Sidebar Start ===== --> */}
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        {/* <!-- ===== Sidebar End ===== --> */}
+
+        {/* <!-- ===== Content Area Start ===== --> */}
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          {/* <!-- ===== Header Start ===== --> */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* <!-- ===== Header End ===== --> */}
+
+          {/* <!-- ===== Main Content Start ===== --> */}
+          <main>
+            <div className="mx-auto max-w-screen-2xl px-4 py-3">
+    
+              <Outlet />
+            </div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+        </div>
+        {/* <!-- ===== Content Area End ===== --> */}
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </div>
+  );
+};
+
+export default DefaultLayout;
